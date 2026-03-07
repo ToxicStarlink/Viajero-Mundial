a6615429c0f9ed90486e39b6efc3d5b1af3cfd4f
Samuel Ponce Ramírez <delabismo8@gmail.com>
2026-03-07 12:29:35 -0600

    update: conexión base de datos y registro de errores

--- conexion.php
+++ conexion.php
@@ -2,13 +2,25 @@
 
 $host = "localhost";
 $user = "root";
-$password = "";
-$db = "mundial_boletos";
+$password = "admin";
+$db = "boletos_futbol";
+$charset = "utf8mb4";
 
-$conn = new mysqli($host,$user,$password,$db);
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset"; //cadena de tectto para decir que queremos conectarnos
+//a mysql, en nuestra compu y a esa base de datos
 
-if($conn->connect_error){
-die("Error de conexión");
+$option = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,//sirve para capturar los errores
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try{
+    $pdo = new PDO($dsn, $user, $password, $option);
+    echo "¡Conexión exitosa a la base de datos!";
+}catch(\PDOException $e){
+    error_log("error de conexión: " . $e->getMessage(),3,"errores.log");
+    die("Error de conexión con la base de datos.");
 }
 
 ?>
--- registro.php
+++ registro.php
@@ -10,45 +10,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     header("Location: inicio.php");
     exit();
 }
-/*
-
-NOTAS PARA BASE DE DATOS
-
-TABLA: usuarios
-
-id_usuario INT AUTO_INCREMENT PRIMARY KEY
-nombre VARCHAR(100)
-apellido VARCHAR(100)
-usuario VARCHAR(100)
-correo VARCHAR(150) UNIQUE
-password VARCHAR(255)
-fecha_registro DATETIME
-
-PROCESO FUTURO:
-
-Conectar base de datos
-
-$conn = new mysqli("localhost","usuario","password","basedatos");
-
-Recibir datos POST
-
-$nombre = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$usuario = $_POST['usuario'];
-$correo = $_POST['correo'];
-$password = $_POST['password'];
-
-Encriptar contraseña
-
-$password_hash = password_hash($password, PASSWORD_DEFAULT);
-
-4 Insertar usuario
-
-INSERT INTO usuarios (nombre,apellido,correo,password,fecha_registro)
-
-5 Redirigir a login.php
-
-*/
 
 ?>
 
